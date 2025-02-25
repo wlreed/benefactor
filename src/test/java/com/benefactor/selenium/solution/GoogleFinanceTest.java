65b0a6978ec7ba01da7529ea3a43ff3d75983524
wlreed <waltr@icloud.com>
2025-02-25 14:17:32 -0800

    Adding third test: "getIncreasingStocks".
    Test is from Friday's interview.

--- src/test/java/com/benefactor/selenium/solution/GoogleFinanceTest.java
+++ src/test/java/com/benefactor/selenium/solution/GoogleFinanceTest.java
@@ -17,60 +17,77 @@ import com.benefactor.selenium.solution.pages.GoogleFinancePage;
  * GoogleFinanceTest - Tests for the Google Finance webpage.
  */
 public class GoogleFinanceTest extends BaseTest {
-    private static final Logger LOG = LogManager.getLogger();
-    GoogleFinancePage gFP;
+        private static final Logger LOG = LogManager.getLogger();
+        GoogleFinancePage gFP;
 
-    @Test(priority = 1)
-    /**
-     * Test that the Google Finance page is loaded and page
-     * title is correct.
-     */
-    public final void openGoogleFinance() {
-        LOG.debug("Running openGoogleFinance...");
-        gFP = new GoogleFinancePage(driver);
-        LOG.debug("Page Title: " + driver.getTitle());
-        String expectedPageTitle = "Google Finance - "
-                + "Stock Market Prices, Real-time Quotes & Business News";
-        Assert.assertTrue(gFP.pageTitle().equals(expectedPageTitle),
-                "Test " + this.getClass() + " Failed to verify page title");
-    }
+        @Test(priority = 1)
+        /**
+         * Test that the Google Finance page is loaded and page
+         * title is correct.
+         */
+        public final void openGoogleFinance() {
+                LOG.debug("Running openGoogleFinance...");
+                gFP = new GoogleFinancePage(driver);
+                LOG.debug("Page Title: " + driver.getTitle());
+                String expectedPageTitle = "Google Finance - "
+                                + "Stock Market Prices, Real-time Quotes & Business News";
+                Assert.assertTrue(gFP.pageTitle().equals(expectedPageTitle),
+                                "Test " + this.getClass() + " Failed to verify page title");
+        }
 
-    @Test(priority = 2)
-    /**
-     * Print out the stocks that are expected but not on the page,
-     * and the stocks that are on the page but not expected.
-     */
-    public final void compareStockSymbols() {
-        LOG.debug("Running compareStockSymbols");
-        Collection<String> expectedStock = Arrays
-                .asList("NFLX", "MSFT", "TSLA");
-        gFP = new GoogleFinancePage(driver);
-        Collection<String> currStock = new ArrayList<>(gFP.getStocks());
-        LOG.debug("Current stocks: " + currStock);
-        LOG.info("Stocks not expected: "
-                + findMissingStrings(currStock, expectedStock));
-        LOG.info("Stocks expected but not found: "
-                + findMissingStrings(expectedStock, currStock));
-        // This test prints out missing strings so just add an Assert
-        // for completion
-        Assert.assertTrue(true,
-                "Test " + this.getClass()
-                        + " Failed to compare Stock Symbols");
-    }
+        @Test(priority = 2)
+        /**
+         * Print out the stocks that are expected but not on the page,
+         * and the stocks that are on the page but not expected.
+         */
+        public final void compareStockSymbols() {
+                LOG.debug("Running compareStockSymbols");
+                Collection<String> expectedStock = Arrays
+                                .asList("NFLX", "MSFT", "TSLA");
+                gFP = new GoogleFinancePage(driver);
+                Collection<String> currStock = new ArrayList<>(gFP.getStocksByTicker());
+                LOG.debug("Current stocks: " + currStock);
+                LOG.info("Stocks not expected: "
+                                + findMissingStrings(currStock, expectedStock));
+                LOG.info("Stocks expected but not found: "
+                                + findMissingStrings(expectedStock, currStock));
+                // This test prints out missing strings so just add an Assert
+                // for completion
+                Assert.assertTrue(true,
+                                "Test " + this.getClass()
+                                                + " Failed to compare Stock Symbols");
+        }
 
-    /**
-     * findMissingStrings compares a Collection of Strings with another
-     * and returns Strings in the first Collection that are not in the
-     * Second.
-     *
-     * @param a - The first Collection
-     * @param b - The second Collection
-     * @return - Strings in the first that are not in the second
-     */
-    public final List<String> findMissingStrings(
-            final Collection<String> a, final Collection<String> b) {
-        List<String> output = new ArrayList<>(a);
-        output.removeAll(b);
-        return output;
-    }
+        @Test(priority = 3)
+        /**
+         * Print out the stocks that are expected but not on the page,
+         * and the stocks that are on the page but not expected.
+         */
+        public final void getIncreasingStocks() {
+                LOG.debug("Running getIncreasingStocks");
+                gFP = new GoogleFinancePage(driver);
+                Collection<String> currStock = new ArrayList<>(gFP.getStocksAllInfo());
+                LOG.info("currStock: " + currStock);
+                // This test prints out missing strings so just add an Assert
+                // for completion
+                Assert.assertTrue(true,
+                                "Test " + this.getClass()
+                                                + " Failed to get Increasing Stocks");
+        }
+
+        /**
+         * findMissingStrings compares a Collection of Strings with another
+         * and returns Strings in the first Collection that are not in the
+         * Second.
+         *
+         * @param a - The first Collection
+         * @param b - The second Collection
+         * @return - Strings in the first that are not in the second
+         */
+        public final List<String> findMissingStrings(
+                        final Collection<String> a, final Collection<String> b) {
+                List<String> output = new ArrayList<>(a);
+                output.removeAll(b);
+                return output;
+        }
 }
--- src/test/java/com/benefactor/selenium/solution/pages/GoogleFinancePage.java
+++ src/test/java/com/benefactor/selenium/solution/pages/GoogleFinancePage.java
@@ -44,16 +44,39 @@ public class GoogleFinancePage {
      *
      * @return - List of Strings.
      */
-    public final List<String> getStocks() {
+    public final List<String> getStocksByTicker() {
         List<String> output = new ArrayList<>();
         // Grab stockList by className
         WebElement stockList = driver.findElement(
                 By.className("sbnBtf"));
         // Inside stockList, get the stocks by className
         List<WebElement> stocks = stockList.findElements(
-                By.className("COaKTb"));
+                By.className("COaKTb")); // gets the stock codes
         for (int i = 0; i < stocks.size(); i++) {
             output.add(stocks.get(i).getText());
+        }
+        return output;
+    }
+
+    /**
+     * getStocks - Gets the stocks from the section of the
+     * page specified and returns them as a List of Strings.
+     *
+     * @return - List of Strings.
+     */
+    public final List<String> getStocksAllInfo() {
+        List<String> output = new ArrayList<>();
+        // Grab stockList by className
+        WebElement stockList = driver.findElement(
+                By.className("sbnBtf"));
+        // Inside stockList, get the stocks by className
+        List<WebElement> stocks = stockList.findElements(
+                By.className("SxcTic")); // gets all data
+        for (int i = 0; i < stocks.size(); i++) {
+            WebElement upDown = stocks.get(i).findElement(By.className("P2Luy"));
+            if (upDown.getText().contains("+$")) {
+                output.add(stocks.get(i).findElement(By.className("COaKTb")).getText());
+            }
         }
         return output;
     }
